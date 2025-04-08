2b7df1b721b34bb12cdc1ad3537441adc02bc8b7
David Barley <ghuitster@gmail.com>
2025-04-08 02:26:37 -0400

    Don't need a home page anymore

--- main.py
+++ main.py
@@ -186,9 +186,5 @@ def assignNames(family):
 	else:
 		return 'That combination of participants and spouses does not have a possible, valid result'
 
-@app.route('/')
-def home():
-	return 'Just a boring home page'
-
 if __name__ == '__main__':
 	app.run(debug=True)
